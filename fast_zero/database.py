35108a4dd20b2828931e361ec6967d442aa612d7
p-mendoncad <mendonca.pedrodias@gmail.com>
2026-01-19 17:07:32 -0300

    chore: organizar estrutura do projeto e unificar .gitignore

---
+++ fast_zero/database.py
@@ -0,0 +1,12 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+
+from fast_zero.settings import Settings
+
+engine = create_engine(Settings().DATABASE_URL)
+
+
+# pragma: no cover
+def get_session():
+    with Session(engine) as session:
+        yield session
